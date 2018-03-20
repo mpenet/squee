d3ea4ecc19d3e1bda56d95425242ad89372a8174
Max Penet <m@qbits.cc>
2018-03-20 13:36:27 +0100

    update package info

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.ghadishayban/squee "0.0.1"
+(defproject cc.qbits/squee "0.0.1"
   :description "A fresh JDBC library"
   :url "http://github.com/ghadishayban/squee"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]]
-  :profiles {:dev {:plugins [[codox "0.8.12"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:plugins [[codox "0.10.3"]]
                    :codox {:include [squee.jdbc squee.datasources squee.impl.protocols]
-                           :src-dir-uri "http://github.com/ghadishayban/squee/blob/master/"
+                           :src-dir-uri "http://github.com/mpenet/squee/blob/master/"
                            :src-linenum-anchor-prefix "L"}}})
